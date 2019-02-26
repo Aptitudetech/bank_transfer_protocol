76ba0cd10a9751e6c1997dba7fbcd86250c64096
Bernard Lefebvre <blefebvre@aptitudetech.net>
2019-02-26 01:56:08 +0000

    Setup.py fix

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
 
 version = '0.0.1'
 requirements = parse_requirements("requirements.txt", session="")
